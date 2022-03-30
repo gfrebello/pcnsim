5412705655bc1df1c6bcb1c0b241978fd7da57af
gFrancoCamilo <gustavocamilo@poli.ufrj.br>
2022-03-30 18:13:04 -0300

    Added readthedocs configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-import myst_parser
 
 # -- Project information -----------------------------------------------------
 
@@ -28,7 +27,8 @@ author = 'Gabriel Rebello and Gustavo Camilo'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser'
+extensions = [
+    "myst_parser",
 ]
 
 from recommonmark.parser import CommonMarkParser
